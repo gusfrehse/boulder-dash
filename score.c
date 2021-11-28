495614b075b0d38c95ed828926682fb0cdf3e8ce
Gustavo S Frehse <gusfrehse@gmail.com>
2021-11-28 14:27:04 -0300

    Fixed score saving when all in the score file are higher.

--- score.c
+++ score.c
@@ -36,38 +36,53 @@ void insert_score(char *path, score hs) {
 	}
 
 	int test_score;
+	int bytes_scanned;
+	int items_scanned;
 
-	while (fscanf(f, "%d %*[^\n] ", &test_score) > 0) {
-		fprintf(stderr, "DEBUG: Comparing %d < %d\n", test_score, hs.score);
-		if (test_score < hs.score)
+	while ((items_scanned = fscanf(f, "%d %*[^\n] %n", &test_score, &bytes_scanned)) > 0) {
+		fprintf(stderr, "DEBUG: Comparing %d < %d. Scanned: %d\n", test_score, hs.score, bytes_scanned);
+		if (test_score < hs.score) {
+			fprintf(stderr, "DEBUG: breaking out of loop\n");
 			break;
+		}
 	}
-
-	// Position that we will write our score at
-	int curr_pos = ftell(f);
 	
-	// End position
-	fseek(f, 0L, SEEK_END);
-	int end_pos = ftell(f);
+	// fscanf buggy, %n is returning the last value even though the fscanf returns -1. 
+	// So we need this if and the code is much worse :/ .
+	if (items_scanned > 0) {
+		// We are after the line we should be, so we go back.
+		fseek(f, -bytes_scanned, SEEK_CUR);
 
-	// The amount of bytes we will have to push back
-	int amount_to_move = end_pos - curr_pos;
+		// Position that we will write our score at
+		int curr_pos = ftell(f);
+		
+		// End position
+		fseek(f, 0L, SEEK_END);
+		int end_pos = ftell(f);
 
-	// Go back to original position to read the data
-	fseek(f, curr_pos, SEEK_SET);
+		// The amount of bytes we will have to push back
+		int amount_to_move = end_pos - curr_pos;
 
-	char *buff = calloc(amount_to_move, 1);
-	if (fread(buff, 1, amount_to_move, f) != amount_to_move) {
-		fprintf(stderr, "ERROR: Could not read score file '%s'\n", path);
-		exit(1);
+		// Go back to original position to read the data
+		fseek(f, curr_pos, SEEK_SET);
+
+		char *buff = calloc(amount_to_move, 1);
+		if (fread(buff, 1, amount_to_move, f) != amount_to_move) {
+			fprintf(stderr, "ERROR: Could not read score file '%s'\n", path);
+			exit(1);
+		}
+
+		// Go back to the original position to write the new data
+		fseek(f, curr_pos, SEEK_SET);
+
+		fprintf(f, "%d %s\n", hs.score, hs.name);
+
+		// Write the old data afterwards
+		fwrite(buff, 1, amount_to_move, f);
+	} else {
+		// We did not find any score smaller so we just append
+		fprintf(f, "%d %s\n", hs.score, hs.name);
 	}
-
-	// Go back to the original position to write the new data
-	fseek(f, curr_pos, SEEK_SET);
-	fprintf(f, "%d %s\n", hs.score, hs.name);
-
-	// Write the old data afterwards
-	fwrite(buff, 1, amount_to_move, f);
 
 	fclose(f);
 	return;
